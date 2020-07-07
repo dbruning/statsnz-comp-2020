d38afc33a3227b0f5c9993fdb52d37bd403adb77
Darren <darren@bruning.net.nz>
2020-07-07 12:02:09 +1200

    Load sass

--- vue.config.js
+++ vue.config.js
@@ -10,6 +10,17 @@ module.exports = {
           header: true,
           skipEmptyLines: true
         }
+      },
+      // https://vue-loader.vuejs.org/guide/pre-processors.html#sass
+      // this will apply to both plain `.scss` files
+      // AND `<style lang="scss">` blocks in `.vue` files
+      {
+        test: /\.scss$/,
+        use: [
+          'vue-style-loader',
+          'css-loader',
+          'sass-loader'
+        ]
       }
     )
     if (process.env.NODE_ENV === 'production') {
